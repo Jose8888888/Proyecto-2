1abae2701e01d255a5e28346c61710108cbe5177
José Alberto Rosales Peña <joserosales@ciencias.unam.mx>
2022-11-08 18:54:18 -0600

    minar

--- Reproductor/app/src/main/java/mx/unam/fciencias/reproductor/MainActivity.kt
+++ Reproductor/app/src/main/java/mx/unam/fciencias/reproductor/MainActivity.kt
@@ -1,34 +1,66 @@
 package mx.unam.fciencias.reproductor
 
 import android.Manifest
-import android.media.MediaMetadataRetriever
+import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.io.File
-
 import androidx.core.app.ActivityCompat
-import android.widget.Toast
-import android.util.Log
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 
 class MainActivity : AppCompatActivity() {
 
-    val minero =  Minero()
+    val minero = Minero()
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1)
+        if (!ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        ) {
+            ActivityCompat.requestPermissions(
+                this@MainActivity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                1
+            )
         }
 
-  }
+        val path = "storage/emulated/0/Download"
+        Log.d("Files", "Path: $path")
 
+
+        val directory = File(path)
+        val files = directory.listFiles()
+        Log.d("Files", "Size: " + files.size)
+        for (i in files.indices) {
+
+            val indice: Int = files[i].getAbsolutePath().lastIndexOf(".")
+
+            if (indice != -1) {
+
+                val extension: String =
+                    files[i].getAbsolutePath().substring(indice)
+
+                if (extension == ".mp3") {
+                    Log.d("Files", "FileName:" + files[i].name)
+
+                }
+            }
+        }
+
+    }
 
 
     fun recibeEntrada(v: View) {
@@ -41,7 +73,5 @@ class MainActivity : AppCompatActivity() {
         println(minero.leeNumero(entrada.getText().toString()))
     }
 
-    //val ruta = "storage/emulated/0/Music/Samsung/Over_the_Horizon.mp3"
-
-
 }
+
